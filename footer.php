c1c1d1fc5ae937413ad39c90ce97349e8a927c4d
Andrey <soul.evans.15@gmail.com>
2018-12-14 01:35:02 +0300

    initial commit with my website

---
+++ footer.php
@@ -0,0 +1,28 @@
+<footer>
+    <ul class="socials">
+        <li><a href="#"><i class="fa fa-vk"></i></a></li>
+        <li><a href="mailto:soul.evans.15@gmail.com"><i class="fa fa-google"></i></a></li>
+        <li><a href="https://freelance.ru/doctorsylar" target="_blank"><i style="font-style: normal">FL</i></a></li>
+        <li><a href="https://github.com/doctorsylar" target="_blank"><i class="fa fa-github"></i></a></li>
+        <li><a href="skype:andrei.94.spb?userinfo"><i class="fa fa-skype"></i></a></li>
+        <li><a href="tg://resolve?domain=DoctorSdoctor"><i class="fa fa-telegram"></i></a></li>
+    </ul>
+    <div class="copyright">
+        © 2018 Andrey Izotov | Inspired by <a href="https://scripteden.com" target="_blank">scripteden</a>
+    </div>
+</footer>
+
+
+<i class="fa fa-times close-modal"></i>
+<div class="overlay"></div>
+<!--  DEVELOPMENT  -->
+<!--<link rel="stylesheet" href="--><?//=DS_ROOT . '/css/bootstrap-grid.min.css'?><!--">-->
+<!--<link rel="stylesheet" href="--><?//=DS_ROOT . '/font-awesome/css/font-awesome.min.css'?><!--">-->
+<!--<link rel="stylesheet" href="--><?//=DS_ROOT . '/style.css'?><!--">-->
+<!--<script src="--><?//=DS_ROOT . '/js/jquery-3.3.1.min.js'?><!--"></script>-->
+<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/easy-pie-chart/2.1.6/jquery.easypiechart.min.js"></script>-->
+<!--<script src="--><?//=DS_ROOT . '/js/script.js'?><!--"></script>-->
+<!--    MINIFIED     -->
+<link rel="stylesheet" href="<?=DS_ROOT . '/minified/index.css'?>">
+<script src="<?=DS_ROOT . '/minified/index.js'?>"></script>
+<link href="https://fonts.googleapis.com/css?family=Oswald|Raleway" rel="stylesheet">
---
+++ page-blog.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Template Name: blog
+*/
+?>
+<?php get_header(); ?>
+<?='blog'?>
+
+<?php get_footer(); ?>
