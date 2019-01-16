694f34c72a05dad28d3156ca552baab5d63d2b5d
Andrey <33748355+doctorsylar@users.noreply.github.com>
2019-01-16 19:41:48 +0300

    changed description printing

--- header.php
+++ header.php
@@ -6,15 +6,15 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="description"
-          content="Я являюсь фрилансером и занимаюсь разработкой сайтов на Wordpress,
-          версткой сайтов, написанием frontend JS-скриптов и программированием
-          серверных PHP-скриптов. Вы можете заказать у меня полностью адаптивный
-          и современный вебсайт, верстку готового макета или работу над скриптами."
-    >
+          content="
+          <?php
+            $content = get_post(get_the_ID());
+            echo apply_filters('the_content', $content->post_content);
+          ?>">
     <meta name="keywords"
           content="заказать сайт, заказ вебсайта, вебсайт, wordpress, разработка вебсайтов,
-          купить сайт, wordpress, красивые сайты, разработка адаптивных сайтов,
-          frontend, backend, fullstack, react, bootstrap"
+          купить сайт, красивые сайты, разработка адаптивных сайтов, вебприложения,
+          frontend, backend, fullstack, react, vue.js, bootstrap"
     >
     <meta name="google-site-verification" content="nc9WZs8kpAm7lB0AbJrsj3FYb9hh8IDApepiiCwS-sc" />
     <title><?php is_front_page() ? bloginfo('description') : wp_title(''); ?> | <?php bloginfo('name'); ?></title>
