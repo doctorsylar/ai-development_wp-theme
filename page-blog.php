c102ceed2c4ba6fdcfb3a35b1ed90fd3b18922b6
Andrey <soul.evans.15@gmail.com>
2019-01-28 21:13:10 +0300

    added logo on blog page, changed colors for fonts and SVGs, devided sections of blog-page

--- page-blog.php
+++ page-blog.php
@@ -4,8 +4,21 @@ Template Name: blog
 */
 ?>
 <?php get_header(); ?>
-<main>
-    blog
-</main>
+
+<aside>
+
+</aside>
+<div class="container">
+    <header>
+        <?php get_home_url()?>
+        <a href="<?= get_home_url()?>" title="Главная страница">
+            <img src="/wp-content/themes/doctorsyl_portfolio/img/blog/logo_blog.svg" alt="logo_blog">
+        </a>
+    </header>
+    <main>
+        blog
+    </main>
+</div>
+
 
 <?php get_footer(); ?>
