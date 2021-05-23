c10f46027015f85b90fb0bfb6891fd0ab5f1327d
doctorsylar <soul.evans.15@gmail.com>
2021-05-24 02:55:25 +0300

    portfolio item minor changes

--- header-portfolio_item.php
+++ header-portfolio_item.php
@@ -4,18 +4,17 @@ get_header();
 <header id="header" class="homepage-header">
     <div class="header-container col-12 col-sm-12 col-md-11 col-lg-10">
         <div class="header-logo">
-            <a href="#home" class="anchor">
+            <a href="<?= get_home_url() ?>" class="anchor">
                 <img src="<?= temp_dir ?>/img/svg/logo.svg" alt="ai-development">
             </a>
         </div>
         <nav class="header-menu horizontal-menu">
             <ul>
-                <li class="link-home"><a href="<?=get_site_url() . '/blog'?>"><i class="fa fa-home"></i>Блог</a></li>
-                <li class="link-services"><a class="anchor" href="#services"><i class="fa fa-files-o"></i>Услуги</a></li>
-                <li class="link-portfolio"><a class="anchor" href="#portfolio"><i class="fa fa-book"></i>Портфолио</a></li>
-                <li class="link-skills"><a class="anchor" href="#skills"><i class="fa fa-star"></i>Навыки</a></li>
-                <li class="link-about"><a class="anchor" href="#about"><i class="fa fa-street-view"></i>Обо мне</a></li>
-                <li class="link-contact"><a class="anchor" href="#contact"><i class="fa fa-envelope-open"></i>Контакты</a></li>
+                <li class="link-services"><a class="" href="<?= get_home_url() ?>#services"><i class="fa fa-files-o"></i>Услуги</a></li>
+                <li class="link-portfolio"><a class="" href="<?= get_home_url() ?>#portfolio"><i class="fa fa-book"></i>Портфолио</a></li>
+                <li class="link-skills"><a class="" href="<?= get_home_url() ?>#skills"><i class="fa fa-star"></i>Навыки</a></li>
+                <li class="link-about"><a class="" href="<?= get_home_url() ?>#about"><i class="fa fa-street-view"></i>Обо мне</a></li>
+                <li class="link-contact"><a class="" href="<?= get_home_url() ?>#contact"><i class="fa fa-envelope-open"></i>Контакты</a></li>
             </ul>
         </nav>
         <div class="menu-toggler">
--- single-portfolio_item.php
+++ single-portfolio_item.php
@@ -1,19 +1,21 @@
 <?php get_header('portfolio_item');
-$fields = get_fields();
+$post->fields = get_fields();
+$prev = get_previous_post();
+$next = get_next_post();
 ?>
 
 <div class="portfolio-item-container main-container">
+    <?php if ($prev !== '' && $prev !== null) : ?>
+        <a title="<?= $prev->post_title ?>" href="<?= get_permalink($prev->ID) ?>" class="portfolio-prev portfolio-arrow"><</a>
+    <?php endif; ?>
+    <?php if ($next !== '' && $next !== null) : ?>
+        <a title="<?= $next->post_title ?>" href="<?= get_permalink($next->ID) ?>" class="portfolio-next portfolio-arrow">></a>
+    <?php endif; ?>
     <main class="main-content col-12">
         <section class="portfolio-item">
             <?php
-            var_dump($post);
-            if ( have_posts() ) { while ( have_posts() ) { the_post(); ?>
-                <!-- Цикл WordPress -->
-                <!-- Вывод постов: the_title() и т.д. -->
-                <div><?php the_content(); ?></div>
-            <?php } } else { ?>
-                <p>Записей нет.</p>
-            <?php } ?>
+//            var_dump($post);
+            ?>
         </section>
     </main>
 </div>
